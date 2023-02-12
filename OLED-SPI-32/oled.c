03bb2302308e0b58315725b14cea7619d802b135
nayooooo <97676318+nayooooo@users.noreply.github.com>
2023-02-12 22:47:12 +0800

    Add files via upload

--- OLED-SPI-32/oled.c
+++ OLED-SPI-32/oled.c
@@ -1175,35 +1175,35 @@ void OLED_Init(void)
 {
 	OLED_GPIO_Init();
 					  
-	OLED_WR_Byte(0xAE,OLED_CMD);
-	OLED_WR_Byte(0xD5,OLED_CMD);
-	OLED_WR_Byte(80,OLED_CMD);  
-	OLED_WR_Byte(0xA8,OLED_CMD);
-	OLED_WR_Byte(0X3F,OLED_CMD);
-	OLED_WR_Byte(0xD3,OLED_CMD);
-	OLED_WR_Byte(0X00,OLED_CMD);
+	OLED_WR_Byte(0xAE,OLED_CMD);  // �ر�OLED��ʾ
+	OLED_WR_Byte(0xD5,OLED_CMD);  // ����ʱ�ӷ�Ƶ���ӡ���Ƶ��
+	OLED_WR_Byte(80,OLED_CMD);    // [3,0]��Ƶ���ӣ�[7,4]��Ƶ��
+	OLED_WR_Byte(0xA8,OLED_CMD);  // ��������·��
+	OLED_WR_Byte(0X3F,OLED_CMD);  // Ĭ��0X3F(1/64)
+	OLED_WR_Byte(0xD3,OLED_CMD);  // ������ʾƫ��
+	OLED_WR_Byte(0X00,OLED_CMD);  // Ĭ��Ϊ0
 
-	OLED_WR_Byte(0x40,OLED_CMD);
+	OLED_WR_Byte(0x40,OLED_CMD);  // ������ʾ��ʼ�У�[5,0]����
 								
-	OLED_WR_Byte(0x8D,OLED_CMD);
-	OLED_WR_Byte(0x14,OLED_CMD);
-	OLED_WR_Byte(0x20,OLED_CMD);
-	OLED_WR_Byte(0x02,OLED_CMD);
-	OLED_WR_Byte(0xA1,OLED_CMD);
-	OLED_WR_Byte(0xC0,OLED_CMD);
-	OLED_WR_Byte(0xDA,OLED_CMD);
-	OLED_WR_Byte(0x12,OLED_CMD);
+	OLED_WR_Byte(0x8D,OLED_CMD);  // ��ɱ�����
+	OLED_WR_Byte(0x14,OLED_CMD);  // bit2����ʼ/�ر�
+	OLED_WR_Byte(0x20,OLED_CMD);  // �����ڴ��ַģʽ
+	OLED_WR_Byte(0x02,OLED_CMD);  // [1,0]��01->�е�ַģʽ��10->�У�ҳ����ַģʽ��Ĭ��10
+	OLED_WR_Byte(0xA1,OLED_CMD);  // ���ض������ã�bit0��0->0��1->127
+	OLED_WR_Byte(0xC0,OLED_CMD);  // ����COMɨ�跽��[3,0]������ѡ����ͨģʽ
+	OLED_WR_Byte(0xDA,OLED_CMD);  // ����COMӲ����������
+	OLED_WR_Byte(0x12,OLED_CMD);  // [5,4]����
 		 
-	OLED_WR_Byte(0x81,OLED_CMD);
-	OLED_WR_Byte(0xEF,OLED_CMD);
-	OLED_WR_Byte(0xD9,OLED_CMD);
-	OLED_WR_Byte(0xf1,OLED_CMD);
-	OLED_WR_Byte(0xDB,OLED_CMD);
-	OLED_WR_Byte(0x30,OLED_CMD);
+	OLED_WR_Byte(0x81,OLED_CMD);  // �Աȶ�����
+	OLED_WR_Byte(0xEF,OLED_CMD);  // �������ã�Խ��Խ����[7,0]��Ĭ��0X7F
+	OLED_WR_Byte(0xD9,OLED_CMD);  // ����Ԥ�������
+	OLED_WR_Byte(0xF1,OLED_CMD);  // [3,0]PHASE 1��[7,4]PHASE 2
+	OLED_WR_Byte(0xDB,OLED_CMD);  // ����VCOMH��ѹ����
+	OLED_WR_Byte(0x30,OLED_CMD);  // [6,4]��000->0.65*VCC��001->0.77*VCC��011->0.83*VCC
 
-	OLED_WR_Byte(0xA4,OLED_CMD);
-	OLED_WR_Byte(0xA6,OLED_CMD);
-	OLED_WR_Byte(0xAF,OLED_CMD);
+	OLED_WR_Byte(0xA4,OLED_CMD);  // ȫ����ʾ������bit0��1->������0->�رգ�����/������
+	OLED_WR_Byte(0xA6,OLED_CMD);  // ������ʾ��ʽ��bit0��1->������ʾ��0->������ʾ
+	OLED_WR_Byte(0xAF,OLED_CMD);  // ������ʾ
 	
 	OLED_Clear();
 }
